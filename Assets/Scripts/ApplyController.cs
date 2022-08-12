45890638e5446790dab16f492318526811d5c515
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-08-12 23:29:05 +0900

    アプリの処理を変更した

---
+++ Assets/Scripts/ApplyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アプリの挙動を制御する(仮)
+/// </summary>
+public class ApplyController : MonoBehaviour
+{
+    /// <summary>
+    /// アプリを終了する
+    /// </summary>
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
