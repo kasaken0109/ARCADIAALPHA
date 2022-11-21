d7f6a1df10a18f198cd5443a3051d58bbafac24d
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-11-21 16:20:13 +0900

    バリアの耐久値が減ったときに表示を変更するように修正した

--- Assets/BarriaDisplay.cs
+++ Assets/BarriaDisplay.cs
@@ -10,8 +10,17 @@ public class BarriaDisplay : MonoBehaviour,IDamage
     [SerializeField]
     Transform _chasePoint = default;
     [SerializeField]
+    GameObject _damage;
+    [SerializeField]
+    GameObject _fragileEffect = default;
+    [SerializeField]
+    GameObject _crush;
+    [SerializeField]
     UnityEvent _hitEvent;
+    [SerializeField]
+    Material _damaged = default;
 
+    Renderer renderer = default;
     int hp = 0;
 
     public void AddDamage(int damage, ref GameObject call)
@@ -19,19 +28,30 @@ public class BarriaDisplay : MonoBehaviour,IDamage
         if(hp > damage)
         {
             hp -= damage;
+            Instantiate(_damage, transform.position, transform.rotation);
+            _fragileEffect.SetActive(hp == 1);
         }
         else
         {
             hp = 0;
+            Instantiate(_crush,transform.position,transform.rotation);
             gameObject.SetActive(false);
         }
         _hitEvent?.Invoke();
     }
 
+    private void Start()
+    {
+        renderer = GetComponentInChildren<Renderer>();
+    }
     // Start is called before the first frame update
     private void OnEnable()
     {
         hp = _barriaHp;
+    }
+    private void OnDisable()
+    {
+        _fragileEffect.SetActive(false);
     }
 
     // Update is called once per frame
