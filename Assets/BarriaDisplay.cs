03e34b3fa75eebf08fbe903fe791f0be89132d80
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-11-26 23:55:56 +0900

    バリアの体力処理を修正した

--- Assets/BarriaDisplay.cs
+++ Assets/BarriaDisplay.cs
@@ -3,28 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+/// <summary>
+/// バリアの体力、表示を管理する
+/// </summary>
 public class BarriaDisplay : MonoBehaviour,IDamage
 {
     [SerializeField]
+    [Tooltip("体力")]
     int _barriaHp = 2;
     [SerializeField]
+    [Tooltip("バリアオブジェクトの表示位置")]
     Transform _chasePoint = default;
     [SerializeField]
-    GameObject _damage;
+    [Tooltip("耐久値減少時に発生するエフェクト")]
+    GameObject _damage = default;
     [SerializeField]
+    [Tooltip("耐久値減少時に変化するShaderオブジェクト")]
     GameObject _fragileEffect = default;
     [SerializeField]
-    GameObject _crush;
+    [Tooltip("破壊時に発生するエフェクト")]
+    GameObject _crush = default;
     [SerializeField]
-    UnityEvent _hitEvent;
-    [SerializeField]
-    Material _damaged = default;
+    [Tooltip("破壊時に起こるイベント")]
+    UnityEvent _hitEvent = null;
 
-    Renderer renderer = default;
+    /// <summary>現在の体力</summary>
     int hp = 0;
 
+    /// <summary>
+    /// バリアの体力減少時の処理
+    /// ※バリアのIDamageインターフェースの実装
+    /// </summary>
+    /// <param name="damage">与えるダメージ</param>
+    /// <param name="call">呼んだオブジェクト</param>
     public void AddDamage(int damage, ref GameObject call)
     {
+        //ダメージを与えると耐久値と表示変更、耐久値が無くなったらバリア無効化
         if(hp > damage)
         {
             hp -= damage;
@@ -39,24 +53,21 @@ public class BarriaDisplay : MonoBehaviour,IDamage
         }
         _hitEvent?.Invoke();
     }
-
-    private void Start()
-    {
-        renderer = GetComponentInChildren<Renderer>();
-    }
-    // Start is called before the first frame update
+    
     private void OnEnable()
     {
+        //バリア有効時に耐久値をリセット
         hp = _barriaHp;
     }
     private void OnDisable()
     {
+        //バリア破壊時にひび割れエフェクトを無効化
         _fragileEffect.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //バリアを付けたオブジェクトの位置を追従させる
         transform.position = _chasePoint.position;
     }
 }
