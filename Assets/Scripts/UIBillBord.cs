daf35ef66ceb1a50faf5919227d5a10f3d157035
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-12-02 23:37:37 +0900

    エフェクトの表示処理の変更を行った

--- Assets/Scripts/UIBillBord.cs
+++ Assets/Scripts/UIBillBord.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class UIBillBord : MonoBehaviour
 {
+    [SerializeField]
+    bool IsUpdate = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var rotation = transform.rotation * Quaternion.Euler(0, Camera.main.transform.rotation.y, 0);// * Quaternion.Euler(0, 180, 0);/*transform.rotation * Camera.main.transform.rotation * Quaternion.Euler(0,180,0);*/
+        transform.rotation = rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.rotation = Camera.main.transform.rotation;
+        if (IsUpdate)
+        {
+            //var rotation = transform.rotation * Quaternion.Euler(0, Camera.main.transform.rotation.y, 0);// * Quaternion.Euler(0, 180, 0);/*transform.rotation * Camera.main.transform.rotation * Quaternion.Euler(0,180,0);*/
+            transform.LookAt(Camera.main.transform);
+        }
     }
 }
