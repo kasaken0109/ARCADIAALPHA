73b47cdeba30f13a99d383efabc677ed00b66770
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-12-03 16:34:59 +0900

    処理不可のインターフェースを実装した

--- Assets/Scripts/UI/UIAnimationController.cs
+++ Assets/Scripts/UI/UIAnimationController.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using DG.Tweening;
 
-public class UIAnimationController : MonoBehaviour
+public class UIAnimationController : MonoBehaviour,IUnhinderable
 {
     [SerializeField]
     float _animTime = 1f;
+    [SerializeField]
+    RectTransform _hidePos;
+    [SerializeField]
+    RectTransform _displayPos;
 
     bool _isAnimEnd = false;
     RectTransform rectTransform;
@@ -20,13 +25,20 @@ public class UIAnimationController : MonoBehaviour
     public virtual void Active()
     {
         _isAnimEnd = false;
-        rectTransform.DOScale(Vector3.one, _animTime).OnComplete(() => _isAnimEnd = true);
+        rectTransform.DOMove(_displayPos.position, _animTime).OnComplete(() => _isAnimEnd = true);
     }
 
     public virtual void NonActive()
     {
         _isAnimEnd = false;
-        rectTransform.DOScale(Vector3.zero, _animTime).OnComplete(() => _isAnimEnd = true);
-        gameObject.SetActive(false);
+        rectTransform.DOMove(_hidePos.position, _animTime).OnComplete(() => {
+            _isAnimEnd = true;
+            gameObject.SetActive(false);
+            });
+    }
+
+    public virtual bool IsHinderable()
+    {
+        return _isAnimEnd;
     }
 }
