b57d5b8c0b68e25b6eb7879fe7d4f233a178c885
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-12-06 20:29:34 +0900

    装備のハイライトを行う処理を変更した

---
+++ Assets/SelectFrameController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class SelectFrameController : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform[] _movePoints = default;
+    [SerializeField]
+    GameObject _frame = default;
+    [SerializeField]
+    float _duraration = 0.5f;
+    RectTransform moveRect = default;
+
+    bool IsComoleted;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ServiceLocator.SetInstance<SelectFrameController>(this);
+        _frame.TryGetComponent(out moveRect);
+    }
+
+    private void OnDestroy()
+    {
+        ServiceLocator.RemoveInstance<SelectFrameController>();
+    }
+
+    public void MoveFrame(int posId)
+    {
+        if(posId < 0 || posId >= _movePoints.Length)
+        {
+            Debug.LogError("outBounds_!");
+            return;
+        }
+        moveRect.DOMove(_movePoints[posId].position, _duraration).SetEase(Ease.Linear).WaitForStart();
+    }
+}
