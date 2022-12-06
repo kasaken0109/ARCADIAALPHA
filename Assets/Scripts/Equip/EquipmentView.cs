22ccbda8c590ea42f386495586cd518adcf7d3dc
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-12-06 20:31:14 +0900

    弾の情報を渡す処理を追加した

--- Assets/Scripts/Equip/EquipmentView.cs
+++ Assets/Scripts/Equip/EquipmentView.cs
@@ -9,29 +9,53 @@ public class EquipmentView : MonoBehaviour
     [Tooltip("表示UIリスト")]
     EquipmentInformation[] _equipmentInformations = default;
 
+    [SerializeField]
+    EquipmentDetailInformation[] _equipmentDetailInformations = default;
+
     [Header("初期化データ")]
     [Header("―――――")]
     [SerializeField]
     Sprite _defaultImage = default;
 
     [SerializeField]
-    BulletList _initList = default;
-
-    [SerializeField]
     string _defaultText = "未設定";
 
+    private void Awake()
+    {
+        ServiceLocator.SetInstance<EquipmentView>(this);
+    }
     void Start()
     {
         SetInformations();
     }
     public void SetInformation(int id)
     {
-        if (!EquipmentManager.Instance.Equipments[id]) EquipmentManager.Instance.Equipments[id] = _initList.Bullets[id];
         var instance = EquipmentManager.Instance.Equipments[id];
+        //
         _equipmentInformations[id].BulletImage.sprite = instance ? instance.Image : _defaultImage;
         _equipmentInformations[id].BulletName.text = instance ? instance.Name : _defaultText;
         _equipmentInformations[id].SkillName.text = instance ? instance.passiveSkill ? instance.PassiveSkill.SkillName : _defaultText : _defaultText;
         _equipmentInformations[id].SkillImage.sprite = instance ? instance.passiveSkill ? instance.PassiveSkill.ImageBullet : _defaultImage : _defaultImage;
+    }
+
+    public void SetBulletDetailExplainInformations()
+    {
+        var instance = ServiceLocator.GetInstance<EquipDataPresenter>();
+        for (int i = 0; i < instance.GetBulletLength(); i++)
+        {
+            _equipmentDetailInformations[i].BulletName.text = instance.GetBulletData(i).Name;
+            _equipmentDetailInformations[i].BulletExplainContext.text = instance.GetBulletData(i).ExplainText;
+        }
+    }
+
+    public void SetSkillDetailExplainInformations()
+    {
+        var instance = ServiceLocator.GetInstance<EquipDataPresenter>();
+        for (int i = 0; i < instance.GetBulletLength(); i++)
+        {
+            _equipmentDetailInformations[i].SkillName.text = instance.GetSkillData(i).SkillName;
+            _equipmentDetailInformations[i].SkillExplainContext.text = instance.GetSkillData(i).ExplainText;
+        }
     }
 
     /// <summary>
@@ -43,5 +67,10 @@ public class EquipmentView : MonoBehaviour
         {
             SetInformation(i);
         }
+    }
+
+    private void OnDestroy()
+    {
+        ServiceLocator.RemoveInstance<EquipmentView>();
     }
 }
