601363fd258de28ef37a7546338928c0dcc212e3
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-12-13 19:23:42 +0900

    エフェクトの遅延表示処理を変更した

--- Assets/Scripts/EffectDelayDisplayer.cs
+++ Assets/Scripts/EffectDelayDisplayer.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// エフェクトの表示タイミングを調整する
+/// </summary>
 public class EffectDelayDisplayer : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("発生させるエフェクト")]
     GameObject _effect = default;
     [SerializeField]
+    [Tooltip("発生を遅らせる時間")]
     float _delayTime = default;
 
+    /// <summary>エフェクトの破棄までの待機時間</summary>
     public float DelayDestroy { set; get; }
     // Start is called before the first frame update
     void Start()
@@ -17,8 +23,7 @@ public class EffectDelayDisplayer : MonoBehaviour
         {
             yield return new WaitForSeconds(_delayTime);
             var m = Instantiate(_effect,transform);
-            yield return new WaitForSeconds(DelayDestroy);
-            Destroy(gameObject);
+            Destroy(gameObject, DelayDestroy);
         }
         StartCoroutine(Spawn());
     }
