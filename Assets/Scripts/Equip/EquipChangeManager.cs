6c438840541d7792405cc87f43e3285ea85479e8
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-12-16 23:48:01 +0900

    UIのアニメーション処理を変更した

--- Assets/Scripts/Equip/EquipChangeManager.cs
+++ Assets/Scripts/Equip/EquipChangeManager.cs
@@ -37,6 +37,7 @@ public class EquipChangeManager : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        ServiceLocator.SetInstance(this);
         cameraSwitcher = FindObjectOfType<DisplayCameraSwitcher>();
         cameraSwitcher.SetFocus(FocusState.Lona);
         SetState(3);
@@ -78,6 +79,10 @@ public class EquipChangeManager : MonoBehaviour
             {
                 case EquipSceneState.EquipMain:
                     _unlockPanel.SetActive(false);
+                    var ui1 = _uIAnimations[0] as UIGroupAnimationController;
+                    ui1.ResetAnims();
+                    var ui2 = _uIAnimations[1] as UIGroupAnimationController;
+                    ui2.ResetAnims();
                     var target = (_sceneState == EquipSceneState.BulletSelect)? _uIAnimations[0] : _uIAnimations[1];
                     _defaultPanel[0].SetActive(true);
                     _panelAnimation.Active();
@@ -98,7 +103,7 @@ public class EquipChangeManager : MonoBehaviour
                     {
                         _panelAnimation.NonActive();
                         unhinderables.Add(_panelAnimation.GetComponent<IUnhinderable>());
-                        _uIAnimations[0].gameObject.SetActive(true);
+                        _uIAnimations[0].Active();
                         ServiceLocator.GetInstance<EquipDataPresenter>().InitUnlock();
                         ServiceLocator.GetInstance<EquipmentView>().SetEquipIcons();
                         _panelAnimation.SetEnableDisplayChange(true);
@@ -108,11 +113,13 @@ public class EquipChangeManager : MonoBehaviour
                     }
                     break;
                 case EquipSceneState.SkillSelect:
+                    ServiceLocator.GetInstance<EquipSkillDisplay>().Reset();
+                    ServiceLocator.GetInstance<EquipSkillDisplay>().SetSkillDisplay();
                     if (_sceneState == EquipSceneState.EquipMain)
                     {
                         _panelAnimation.NonActive();
                         unhinderables.Add(_panelAnimation.GetComponent<IUnhinderable>());
-                        _uIAnimations[1].gameObject.SetActive(true);
+                        _uIAnimations[1].Active();
                         _panelAnimation.SetEnableDisplayChange(true);
                         unhinderables.Add(_uIAnimations[1].GetComponent<IUnhinderable>());
                         prevSelected = EventSystem.current.currentSelectedGameObject;
@@ -135,5 +142,10 @@ public class EquipChangeManager : MonoBehaviour
         yield return new WaitForSeconds(0.4f);
         //ServiceLocator.GetInstance<SelectFrameController>().MoveFrame(id);
     }
-    
+
+    private void OnDestroy()
+    {
+        ServiceLocator.RemoveInstance<EquipChangeManager>();
+    }
+
 }
