072813206947f46b4ac134e75fd48df4a2b78c5c
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2023-01-03 23:39:16 +0900

    文字送りをするクラスを作成した

---
+++ Assets/Scripts/Text/TextSender.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Text.RegularExpressions;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UniRx;
+
+public class TextSender : MonoBehaviour
+{
+    [SerializeField]
+    float _sendTime = 1f;
+    Text _display = default;
+    private void Awake()
+    {
+        TryGetComponent(out _display);
+        SendText("Ç±ÇÍÇÕ_<color=green>ÉJÉCÉäÉÖÅ[</color>_Ç≈Ç∑");
+        Observable.Timer(TimeSpan.FromSeconds(0.5f)).Subscribe(_ => SkipText());
+    }
+
+    bool isStart = true;
+    string skipWord;
+    public void SendText(string displayText)
+    {
+        IEnumerator DelayShowText(string text)
+        {
+            _display.text = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (isSkipped)
+                {
+                    _display.text = text.Replace("_","");
+                    isSkipped = false;
+                    yield break;
+                }
+                if (text[i] == '_' && isStart)
+                {
+                    isStart = false;
+                    continue;
+                }
+                if (!isStart)
+                {
+                    if (text[i] == '_')
+                    {
+                        _display.text += skipWord;
+                        yield return new WaitForSeconds(_sendTime);
+                        skipWord = "";
+                        isStart = true;
+                        continue;
+                    }
+                    skipWord += text[i];
+                    continue;
+                }
+                _display.text += text[i];
+                yield return new WaitForSeconds(_sendTime);
+            }
+        }
+        StartCoroutine(DelayShowText(displayText));
+    }
+
+    bool isSkipped = false;
+    public void SkipText()
+    {
+        isSkipped = true;
+    }
+}
