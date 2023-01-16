88e439cb8bbe70aea66a931bf8240edab5776d69
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2023-01-16 23:55:11 +0900

    結果画面の処理を変更した

--- Assets/Scripts/ResultDisplay.cs
+++ Assets/Scripts/ResultDisplay.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+using DG.Tweening;
 
 /// <summary>
 /// リザルト画面の処理を行う
@@ -11,41 +11,56 @@ public class ResultDisplay : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("タイム表示用のテキスト")]
-    TextMeshProUGUI m_time = null;
+    Text _timeDisplay = default;
 
     [SerializeField]
-    [Tooltip("ランク表示用のテキスト")]
-    TextMeshProUGUI m_rank = null;
+    [Tooltip("ランクのテキストオブジェクト")]
+    GameObject _rankLabel = default;
+
+    [SerializeField]
+    [Tooltip("アニメーションさせる画像オブジェクト")]
+    RectTransform[] _moveObjs = default;
+
+    [SerializeField]
+    [Tooltip("フェードアニメーションさせる画像アイコン")]
+    Image _fadeIcon = default;
+
+    [SerializeField]
+    [Tooltip("ランク表示用の画像ホルダー")]
+    Image _rank = default;
+
+    [SerializeField]
+    [Tooltip("獲得ポイント表示用のテキスト")]
+    Image _point = default;
+
+    [SerializeField]
+    [Tooltip("ランクの画像素材")]
+    Sprite[] _rankImageSources = default;
 
     [SerializeField]
     [Tooltip("シーン遷移用のボタン")]
-    GameObject[] m_buttons;
+    GameObject[] _buttons;
+
+    [SerializeField]
+    [Tooltip("スライドインのアニメーション時間")]
+    float _slideInDuraration = 1f;
 
     [SerializeField]
     [Tooltip("シーン遷移用のボタン表示まで待つ時間")]
-    float m_waitDisplay = 1f;
+    float _waitDisplay = 1f;
 
     /// <summary>クリアまでにかかった時間</summary>
     int clearTime;
-    /// <summary>制限時間</summary>
-    int maxTime;
-    /// <summary>ランクの文字列</summary>
-    string rank;
+    /// <summary>獲得ポイント</summary>
+    int getPoint;
     /// <summary>ランクを判別する数値</summary>
     int clearRank;
-    /// <summary>delta値</summary>
-    const float delta = 0.01f;
-    /// <summary>ランク計算用係数</summary>
-    const int calcValue = 10;
-    /// <summary>deltaTimeのWaitForSeconds</summary>
-    WaitForSeconds deltaTime = new WaitForSeconds(delta);
     /// <summary>表示遅延用のWaitForSeconds</summary>
     WaitForSeconds delayDisplay = new WaitForSeconds(0.5f);
 
     void Start()
     {
         SetRank();
-        StartCoroutine(nameof(DisplayResult));
     }
 
     /// <summary>
@@ -58,63 +73,53 @@ public class ResultDisplay : MonoBehaviour
         //maxTime = PlayerPrefs.GetInt("MaxTime");
         var calc = Mathf.CeilToInt((float)clearTime / 15f);
         //ランク計算
-        clearRank = clearTime < 0 ? 6
+        clearRank = clearTime < 0 ? 4
                     : calc <= 1 ? 0
                     : calc <= 2 ? 1
                     : calc <= 4 ? 2
-                    : calc <= 8 ? 3
-                    : calc <= 16 ? 4
-                    : 5;//(clearTime * calcValue) / maxTime;
-        EquipmentManager.Instance.Point += clearRank < 2 ? 2 : clearRank != 6 ? 1 : 0;
-        switch (clearRank)
-        {
-            case 0:
-                rank = "SS";
-                break;
-            case 1:
-                rank = "S";
-                break;
-            case 2:
-                rank = "A";
-                break;
-            case 3:
-                rank = "B";
-                break;
-            case 4:
-                rank = "C";
-                break;
-            case 5:
-                rank = "D";
-                break;
-            case 6:
-                rank = "Failed...";
-                break;
-        }
+                    : 3;//(clearTime * calcValue) / maxTime;
+        getPoint = clearRank < 2 ? 2 : clearRank != 4 ? 1 : 0;
+        EquipmentManager.Instance.Point += getPoint;
+        _rank.sprite = _rankImageSources[clearRank];
+        _rank.enabled = false;
     }
+
+    string ConvertDisplayTimeText(int time)
+    {
+        int seconds = time % 60;
+        int minutes = time / 60;
+        int hours = time / 3600;
+        return $"{hours}:{minutes}:{seconds}";
+    }
+
+
 
     /// <summary>
     /// クエスト結果を表示する
     /// </summary>
     /// <returns></returns>
-    IEnumerator DisplayResult()
+    public void DisplayResult()
     {
-        float time = 0;
-        //スロット演出
-        while (time < m_waitDisplay)
-        {
-            m_time.text = Random.Range(1, 1000).ToString();
-            time += delta;
-            yield return deltaTime;
-        }
-        //ランク表示
-        m_time.text = clearTime < 0 ? "----" : clearTime.ToString();
-        yield return delayDisplay;
-        m_rank.text = rank;
-        //シーン遷移用ボタン表示
-        foreach (var item in m_buttons)
-        {
-            yield return delayDisplay;
-            item.SetActive(true);
-        }
+        Sequence anim = DOTween.Sequence()
+            .Append(_moveObjs[0].DOLocalMoveY(0, _slideInDuraration))
+            .Join(_moveObjs[1].DOLocalMoveY(0, _slideInDuraration))
+            .Join(_fadeIcon.DOFade(1f, _slideInDuraration))
+            .AppendInterval(_waitDisplay)
+            .Append(_moveObjs[2].DOLocalMoveX(0, _slideInDuraration))
+            .AppendInterval(_waitDisplay)
+            .AppendCallback(() => _timeDisplay.text = ConvertDisplayTimeText(clearTime))
+            .AppendInterval(_waitDisplay)
+            .Append(_moveObjs[3].DOLocalMoveX(0, _slideInDuraration))
+            .AppendCallback(() => _timeDisplay.text = ConvertDisplayTimeText(clearTime))
+            .AppendInterval(_waitDisplay)
+            .AppendCallback(() => _rankLabel.SetActive(true))
+            .AppendInterval(_waitDisplay)
+            .AppendCallback(() => _rank.enabled = true)
+            .AppendInterval(_waitDisplay)
+            .AppendCallback(() =>
+            {
+                _buttons[0].SetActive(true);
+                _buttons[1].SetActive(true);
+            });
     }
 }
