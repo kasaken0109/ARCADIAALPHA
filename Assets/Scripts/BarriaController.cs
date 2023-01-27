0c0000db664ef09de4e6fb3bf1380182fbc5e967
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2023-01-27 23:47:09 +0900

    バリアの処理を追加した

--- Assets/Scripts/BarriaController.cs
+++ Assets/Scripts/BarriaController.cs
@@ -11,22 +11,24 @@ public class BarriaController : MonoBehaviour,ICustomSkillEvent
     bool isHitFromEnemy = false;
 
     GameObject hitObj;
+    Transform hitPoint;
 
-    public Action<GameObject> CustomSkillEvent { get => _customSkillEvent; set => _customSkillEvent = value; }
-    Action<GameObject> _customSkillEvent;
+    public Action<GameObject,Transform> CustomSkillEvent { get => _customSkillEvent; set => _customSkillEvent = value; }
+    Action<GameObject,Transform> _customSkillEvent;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag(_opponentTag))
         {
             hitObj = other.gameObject;
+            hitPoint = other.gameObject.transform;
             isHitFromEnemy = true;
         }
     }
     public void ActiveCustomSkill()
     {
         if (!isHitFromEnemy) return;       
-        _customSkillEvent?.Invoke(hitObj);
+        _customSkillEvent?.Invoke(hitObj,hitPoint);
         isHitFromEnemy = false;
     }
 
