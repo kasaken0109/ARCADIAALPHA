6eec70a2c9f14f8f7ce1be9f6e93d229ca591522
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2023-02-01 23:50:44 +0900

    シーンの処理を変更した

--- Assets/Scripts/ApplyController.cs
+++ Assets/Scripts/ApplyController.cs
@@ -7,12 +7,50 @@ using UnityEngine;
 /// </summary>
 public class ApplyController : MonoBehaviour
 {
+    [SerializeField]
+    string titleSceneName = "TitleScene";
+    [SerializeField]
+    Bullet[] bullets = default;
     /// <summary>
     /// アプリを終了する
     /// </summary>
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void OnBackTitleScene()
+    {
+        InitApplication();
+        SceneLoader.Instance.SceneLoad(titleSceneName);
+    }
+
+    public void InitApplication()
+    {
+        PlayerPrefs.SetInt("IsFirst", 0);
+        PlayerPrefs.Save();
+        for (int i = 0; i < bullets.Length; i++)
+        {
+            bullets[i].IsUnlock = false;
+            bullets[i].PassiveSkill = null;
+        }
+        var manager = EquipmentManager.Instance;
+        manager.Point.Value = 3;
+        for (int i = 0; i < 3; i++)
+        {
+            manager.SetEquipID(i);
+            manager.SetEquipments(null);
+        }
+        manager.SetEquipID(0);
+        //ServiceLocator.GetInstance<EquipmentView>().InitIcons();
+    }
+
+    private void Start()
+    {
+        if(PlayerPrefs.GetInt("IsFirst") == 0)
+//#if UNITY_EDITOR
+        InitApplication();
+//#endif
     }
 
 }
