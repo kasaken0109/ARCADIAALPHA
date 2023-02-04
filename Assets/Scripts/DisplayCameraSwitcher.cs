9455849e2ee5d0d667f26ed715e33bcebd19df8e
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2023-02-04 23:50:04 +0900

    カメラのズーム処理を変更した

--- Assets/Scripts/DisplayCameraSwitcher.cs
+++ Assets/Scripts/DisplayCameraSwitcher.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using System.Linq;
 
 public enum FocusState
 {
@@ -13,13 +14,32 @@ public class DisplayCameraSwitcher : MonoBehaviour
     [SerializeField]
     CinemachineVirtualCameraBase[] _focusCameras = default;
 
-    const int DefaultPriority = 10;
-    const int HighPriority = 20;
-    public void SetFocus(FocusState focusState)
+    const int DefaultPriority = 0;
+    const int HighPriority = 100;
+
+    int[] originPrioritys;
+
+    private void Awake()
+    {
+        originPrioritys = new int[_focusCameras.Length];
+        for (int i = 0; i < originPrioritys.Length; i++)
+        {
+            originPrioritys[i] = _focusCameras[i].Priority;
+        }
+    }
+    public void SetFocus(int focusState)
     {
         for (int i = 0; i < _focusCameras.Length; i++)
         {
-            _focusCameras[i].Priority = i == (int)focusState ? HighPriority : DefaultPriority;
+            _focusCameras[i].Priority = i == (int)focusState ? HighPriority : originPrioritys[i];
+        }
+    }
+
+    public void ResetFocus(int focusState)
+    {
+        for (int i = 0; i < _focusCameras.Length; i++)
+        {
+            _focusCameras[focusState].Priority = originPrioritys[i];
         }
     }
 }
