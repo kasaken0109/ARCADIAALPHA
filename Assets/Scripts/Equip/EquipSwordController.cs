214354d2defa9ff5bc905a7954e6175b8d7ee602
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2023-02-16 23:57:29 +0900

    装備のアウトライン処理方法を変更した

--- Assets/Scripts/Equip/EquipSwordController.cs
+++ Assets/Scripts/Equip/EquipSwordController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
 
 public class EquipSwordController : MonoBehaviour
 {
@@ -10,21 +11,37 @@ public class EquipSwordController : MonoBehaviour
     [SerializeField]
     GameObject[] _swordsEquip;
     [SerializeField]
+    Sprite[] _displayImages = default;
+    [SerializeField]
     Image _currentImage = default;
     [SerializeField]
     SwordInformation[] _swordInformations = default;
     [SerializeField]
     Transform _dispalyParentPos = default;
+    [SerializeField]
+    EquipButtonSelect _buttonSelect = default;
 
     GameObject _currentSword = null;
     int id = 0;
 
     private void Awake()
     {
-        ServiceLocator.SetInstance<EquipSwordController>(this);
+        ServiceLocator.SetInstance(this);
+    }
+
+    private void Start()
+    {
+        _buttonSelect.IsSelect.Subscribe(x => SetHighlightImage(x)).AddTo(this);
+        EquipSwordWeapon();
+    }
+
+    void SetHighlightImage(bool isHighlight)
+    {
+        _currentImage.sprite = _displayImages[id + (isHighlight ? _displayImages.Length / 2 : 0)];
     }
     public void SetWeapon(bool isPrev)
     {
+        SoundManager.Instance.PlayClick(5);
         id = isPrev ?
                 (id == 0 ? _swords.Length - 1 : id -= 1) :
                 (id == _swords.Length - 1 ? 0 : id += 1);
@@ -33,6 +50,8 @@ public class EquipSwordController : MonoBehaviour
 
     public void EquipSwordWeapon()
     {
+        SoundManager.Instance.PlayClick(6);
+        var m = FindObjectOfType<TutorialEquipMissionViewModel>();
         for (int i = 0; i < _swordsEquip.Length; i++)
         {
             _swordsEquip[i].SetActive(i == id);
@@ -40,7 +59,11 @@ public class EquipSwordController : MonoBehaviour
         EquipmentManager.Instance.SetSword(_swords[id]);
         if (_currentSword) Destroy(_currentSword);
         _currentSword = Instantiate(_swords[id], _dispalyParentPos);
-        _currentImage.sprite = _swordInformations[id].Image;
+        _currentImage.sprite = _displayImages[id];
+        //if (m && m.GetValue == 7)
+        //{
+        //    FindObjectOfType<TutorialEquipMissionViewModel>().SetSwordEquip();
+        //}
     }
 
     private void OnDestroy()
