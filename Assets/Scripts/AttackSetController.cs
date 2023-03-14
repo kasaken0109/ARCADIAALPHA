5368c138f826897b3890392b6e7821f066fd465a
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2023-03-14 23:27:46 +0900

    攻撃の処理をUniRx方式に変更した

--- Assets/Scripts/AttackSetController.cs
+++ Assets/Scripts/AttackSetController.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+﻿using System;
 using UnityEngine;
 using System.Collections.Generic;
+using UniRx;
 
 /// <summary>
 /// 攻撃用のオブジェクトの処理を行う
@@ -14,6 +15,11 @@ public class AttackSetController : MonoBehaviour
     public List<AttackcolliderController> AttackCollider;
     [SerializeField]
     Animator _anim = default;
+    [SerializeField]
+    GameObject _animObj = default;
+
+    [SerializeField]
+    CameraController _camera = default;
 
     StateController[] _stateController;
     JumpAttackStateController[] _jumpAttackStates;
@@ -26,6 +32,8 @@ public class AttackSetController : MonoBehaviour
 
     PlayerMoveController _playerMove;
 
+    Transform _target = default;
+
 
     void Start()
     {
@@ -35,6 +43,14 @@ public class AttackSetController : MonoBehaviour
         }
         attackColliderCoroutine = new Coroutine[AttackCollider.Count];
         if(IsPlayer)AnimActionSet();
+        _camera.ObserveEveryValueChanged(x => x.OnLockOnTragetchange).Subscribe(x =>
+        {
+            if (_camera.IsLockOn)
+            {
+                _target = x;
+            }
+            else _target = null;
+        }).AddTo(this);
     }
 
     /// <summary>
@@ -47,13 +63,16 @@ public class AttackSetController : MonoBehaviour
         TryGetComponent(out _playerMove);
         foreach (var item in _stateController)
         {
-            item.SetStateEnterAction(() => _playerMove.SetMoveActive(false));
-            item.SetStateExitAction(() => { _playerMove.SetMoveActiveDelay(true, 0.5f); });
+            item.SetStateEnterAction(() => 
+            {
+                if (_target) _animObj.transform.LookAt(new Vector3(_target.transform.position.x, _animObj.transform.position.y, _target.transform.position.z));
+            });
         }
         foreach (var item in _jumpAttackStates)
         {
             item.SetStateEnterAction(() =>
             {
+                if (_target) _animObj.transform.LookAt(new Vector3(_target.transform.position.x, _animObj.transform.position.y, _target.transform.position.z));
                 _playerMove.StartFloat();
                 _anim.SetBool("IsAttackEnd", false);
             });
